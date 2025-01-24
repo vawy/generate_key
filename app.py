856f67c942bb78576cc2986192c47c1ab7a7cc2a
Vasiliy Vigilyanskiy <v.vigilyanskiy@ivpa.ru>
2025-01-24 18:32:33 +0300

    add notes.txt and edit app.py

--- app.py
+++ app.py
@@ -17,25 +17,28 @@ notes = [
     }
 ]
 
-def save_notes_to_file(notes: list[dict], filename):
+def save_notes_to_file(notes, filename):
     """
     Записывает список заметок в текстовый файл, перезаписывая его содержимое.
     """
-    try:
-        # Открываем файл в режиме записи ('w')
-        with open(filename, 'w', encoding='utf-8') as file:
-            for note in notes:
-                # Записываем каждую заметку в файл
-                file.write(f"Имя пользователя: {note.get('username', 'N/A')}\n")
-                file.write(f"Заголовок: {note.get('title', 'N/A')}\n")
-                file.write(f"Описание: {note.get('content', 'N/A')}\n")
-                file.write(f"Статус: {note.get('status', 'N/A')}\n")
-                file.write(f"Дата создания: {note.get('created_date', 'N/A')}\n")
-                file.write(f"Дедлайн: {note.get('issue_date', 'N/A')}\n")
-                file.write("\n")  # Пустая строка между заметками
-        print(f"Заметки успешно сохранены в файл {filename}!")
-    except Exception as e:
-        print(f"Ошибка при сохранении файла: {e}")
+    # Открываем файл в режиме записи ('w')
+    with open(filename, 'w', encoding='utf-8') as file:
+        for note in notes:
+            # Записываем каждую заметку в файл
+            file.write(f"Имя пользователя: {note.get('username', 'N/A')}\n")
+            file.write(f"Заголовок: {note.get('title', 'N/A')}\n")
+            file.write(f"Описание: {note.get('content', 'N/A')}\n")
+            file.write(f"Статус: {note.get('status', 'N/A')}\n")
+            file.write(f"Дата создания: {note.get('created_date', 'N/A')}\n")
+            file.write(f"Дедлайн: {note.get('issue_date', 'N/A')}\n")
+            file.write("\n")  # Пустая строка между заметками
+    print(f"Заметки успешно сохранены в файл {filename}!")
 
 # Сохраняем заметки в файл
 save_notes_to_file(notes, "notes.txt")
+
+
+# git status - проверить статус ваших изменений
+# git add [filename / .] - добавить файл в отслеживаемые
+# git commit -m "comment" - закоммитить изменения
+# git push - запушить изменения в удаленный репозиторий
